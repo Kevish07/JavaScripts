628c42a665681b78bfb27cfd9fe03655d46f4e6b
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2024-09-10 19:39:49 +0530

    All Files Added

---
+++ Basics/Let,var.js
@@ -0,0 +1,52 @@
+// var age = 25;
+
+// if(true) {
+//     console.log(age);
+// }
+
+// function solve() {
+//     var age = 25;
+//     console.log(age);
+// }
+
+// solve();
+
+// function solve() {
+//     var age = 25;
+    
+// }
+// console.log(age);
+// solve(); 
+
+// var x = 132;
+// var x = 341;
+// console.log(x);
+
+// {
+// let a  = 10;
+// console.log(a);
+// }
+
+
+// {
+// let a  = 10;
+// }
+// console.log(a);
+
+// let a = 12;
+// a = "kevish";
+// a = true;
+// a = null;
+
+
+// const a = 45;
+// console.log(a);
+// a = 23;
+// console.log(a);
+
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+numbers.push(4, 5, 6);
+console.log(numbers); 
+
---
+++ Basics/ops,con.js
@@ -0,0 +1,175 @@
+// Arithmetic operations
+
+
+// let a = 20;
+// let b = 30;
+
+// console.log(a + b);
+
+// let a = 20;
+// let b = 30;
+
+// console.log(a - b);
+
+// let a = 20;
+// let b = 30;
+
+// console.log(a * b);
+
+// let a = 20;
+// let b = 30;
+
+// console.log(a / b);
+
+// let a = 20;
+// let b = 30;
+
+// console.log(a % b);
+
+// let a = 2;
+// let b = 3;
+
+// console.log(a ** b);
+
+
+// Unary Operation
+
+
+// let a = 5;
+// console.log(++a);
+
+// let b = 5;
+// console.log(b++);
+
+// let c = 5;
+// console.log(--c);
+
+// let d = 5;
+// console.log(d--);
+
+
+//Assignment operators
+
+
+// let a = 5;
+// a = 5+5;
+//or
+// a += 5;
+// console.log(a);
+
+
+//Comparison operators
+
+
+// console.log( 10 > 5 );
+// console.log( "5" == 5 );
+// console.log( "5" == 5 );
+// console.log( "5" === 5 );
+// console.log( 5 === 5 );
+
+
+//Ternary operator
+
+
+// let age = 23;
+// let eligibility = (age > 18) ? "You can vote!" : "You are a minor";
+// console.log(eligibility);
+
+
+//Logical Operator
+
+// let logic = (true && true && true)
+// console.log(logic)
+
+// let logic = (false && 0 && true)
+// console.log(logic)
+
+// let logic = (true || true || false)
+// console.log(logic)
+
+// let logic = (false || false || false)
+// console.log(logic)
+
+// let logic = (0 || 0 || true)
+// console.log(logic)
+
+// let logic = (1 || 1 || true)
+// console.log(logic)
+
+// let ans = !(true);
+// console.log(ans)
+
+// let ans = !(0);
+// console.log(ans)
+
+
+//Bitwise operator
+
+
+// let bit = ( 2 & 4 )
+// console.log(bit)
+
+// let bit = ( 2 & 2 )
+// console.log(bit)
+
+// let bit = ( 2 | 9 )
+// console.log(bit)
+
+// let bit = ( ~(0) )
+// console.log(bit)
+
+// let bit = ( 4 ^ 8 )
+// console.log(bit)
+
+// let bit = ( 2 ^ 2 )
+// console.log(bit)
+
+// let bit = ( 10 << 2 )
+// console.log(bit)
+
+// let bit = ( 32 >> 1 )
+// console.log(bit)
+
+
+//Conditions
+
+
+// let age = 13;
+// if(age <= 21){
+//     console.log("You are minor")
+// }
+// else{
+//     console.log("You are ready to marriage")
+// }
+
+// let num = 3;
+// let age = 10;
+// if(num == 1){
+//     console.log("a")
+// }
+// else if(num ==2){
+//     console.log("b")
+// }
+// else if (num == 3){
+//     console.log("c")
+//     if(age>=10){
+//         console.log("You are a kid")
+//     }
+// }
+// else{
+//     console.log("Undefine")
+// }
+
+//Switch case
+// let num = 3;
+// switch (num){
+//     case 1: console.log("A")
+//     break;
+//     case 2: console.log("B")
+//     break;
+//     case 3: console.log("C")
+//     break;
+//     case 4: console.log("D")
+//     break;
+//     default:console.log("Enter b/w 1-4")
+// }
