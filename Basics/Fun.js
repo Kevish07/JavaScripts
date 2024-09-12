26f4ea7062f7a0d2a0036e4b7247173cfee9c616
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2024-09-12 11:59:09 +0530

    added on 12 sep 2k24

---
+++ Basics/Fun.js
@@ -0,0 +1,57 @@
+// function sayMyName () {
+//     console.log("The function is working properly")
+// }
+// sayMyName();
+
+// function printCounting() {
+//     for(let i = 1; i<=100; i++){
+//         console.log(i)
+//     }
+// }
+// printCounting();
+
+// function printNum(num){
+//     console.log("The Number is:", num)
+// }
+// printNum(69);
+
+// function avgFinder (num1, num2) {
+//     let tAvg = (num1 +num2)/2;
+//     console.log("The total average is:", tAvg)
+// }
+
+// avgFinder(5,5);
+
+
+
+// returning vales
+
+// function addNum(a,b,c) {
+//     let adding = a + b + c;
+//     return adding;
+// }
+
+// let output = addNum(1,2,3)
+// console.log("Addition of Numbers :", output);
+
+// function fullName(in1, in2){
+//     let connect = in1 + " " + in2;
+//     return connect;
+// }
+// let output = fullName("Yes" ,"Working");
+// console.log(output);
+
+// function getMulti(num1,num2){
+//     return num1*num2;
+// }
+// console.log(getMulti(5,5))
+
+// const square = function(a){
+//     return a**2;
+// }
+// console.log(square(45));
+
+// let power = (x,y) => {
+//     return x**y;
+// }
+// console.log(power(2,5));
---
+++ Loops&Strings/index.js
@@ -0,0 +1,86 @@
+// For Loop
+
+// for(let i = 1; i <= 10; i ++){
+//     console.log("Repeating String")
+// }
+
+// for(let i = 1; i <= 10; i ++){
+//     console.log(i)
+// }
+
+// for(let i = 10; i > 0; i--){
+//     console.log(i)
+// }
+
+// for(let i = 10; i > 0; i--){
+//     if(i == 4){
+//         break;
+//     }
+//     else{
+//     console.log(i)
+//     }
+// }
+
+// for(let i = 10; i > 0; i--){
+//     if(i == 4){
+//         continue;
+//     }
+//     else{
+//     console.log(i)
+//     }
+// }
+
+
+// While Loop
+
+
+// let i = 1;
+// while(i<=10){
+//     console.log("While is going on");
+//     i++;
+
+// }
+
+// let i = 1;
+// while(i<=4){
+//     console.log("In Loop")
+//     if(i==3){
+//         i++;
+//         continue;
+//     }
+//     else{
+//          console.log(i);
+//     i++;
+//     }
+// }
+
+
+
+
+// Strings
+
+// let YourName = "Kaka"
+// let YourCity = 'Punjab'
+// let YourPro = `Making
+// Music`
+// let YourColor = new String("Black")
+
+// console.log(YourName);
+// console.log(YourCity);
+// console.log(YourPro);
+// console.log(YourColor);
+
+// let lang1 = "English";
+// let lang2 = "Hindi";
+
+// console.log(`${lang1} ${lang2}`)
+// console.log(lang1 + lang2)
+
+// console.log(lang1.length)
+// console.log(lang1.toUpperCase())
+// console.log(lang1.substring(2,4))
+
+// let Sentence = "Hello\\ ji\\ what's\\ up";
+// let Words = Sentence.split("\\");
+// console.log(Words);
+// console.log(Words.join(','))
